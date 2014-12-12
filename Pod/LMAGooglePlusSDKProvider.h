27e14b13ddd55233b04ea77addbf9ee71b109e43
Alexey Aleshkov <djmadcat@gmail.com>
2014-12-13 01:08:05 +0300

    Initial commit

---
+++ Pod/LMAGooglePlusSDKProvider.h
@@ -0,0 +1,20 @@
+//
+//  LMAGooglePlusSDKProvider.h
+//  LetMeAuth-GooglePlusSDK-iOS
+//
+//  Created by Evgeniy Yurtaev on 21.11.14.
+//  Copyright (c) 2014 Webparadox, LLC. All rights reserved.
+//
+
+
+#import <Foundation/Foundation.h>
+#import <LetMeAuth/LetMeAuth.h>
+
+
+extern NSString *const LMAGooglePlusSDKClientId;
+extern NSString *const LMAGooglePlusSDKScope;
+
+
+@interface LMAGooglePlusSDKProvider : NSObject <LMAProvider>
+
+@end
